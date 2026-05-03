f14449aee133c09118b2878413725fe1e2c7e3e8
oneul0 <neul.dev@gmail.com>
2026-05-03 13:30:29 +0900

    필수문제 9문제

--- v2.2/4-5주간문제 디펜스/[LTC] Challenge III - 2/oneul0.java
+++ v2.2/4-5주간문제 디펜스/[LTC] Challenge III - 2/oneul0.java
@@ -1,8 +1,27 @@
-import java.io.*;
-import java.util.*;
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        Deque<int[]> stack = new ArrayDeque<>();
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peekLast()[0] == c) {
+                stack.peekLast()[1]++;
+                if (stack.peekLast()[1] == k) {
+                    stack.pollLast();
+                }
+            } else {
+                stack.offerLast(new int[]{c, 1});
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int[] entry : stack) {
+            char ch = (char) entry[0];
+            int count = entry[1];
+            while (count-- > 0) {
+                sb.append(ch);
+            }
+        }
+
+        return sb.toString();
     }
 }
