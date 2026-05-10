de03966e2e85175f959545e683d6bd4ce524a179
oneul0 <neul.dev@gmail.com>
2026-05-10 14:11:50 +0900

    필수문제 9문제

--- v2.2/5-1주간문제 디펜스/[LTC] Ascension I - 1/oneul0.java
+++ v2.2/5-1주간문제 디펜스/[LTC] Ascension I - 1/oneul0.java
@@ -1,8 +1,26 @@
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        //스택에 인덱스만 넣기
+        //set에 없애야할 괄호 인덱스 넣기
+        Deque<Integer> stack = new ArrayDeque<>();
+        Set<Integer> invalid = new HashSet<>();
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i) == '('){
+                stack.offerLast(i);
+            }
+            else if(s.charAt(i) == ')'){
+                if(stack.isEmpty()) invalid.add(i);
+                stack.pollLast();
+            }
+        }
+        while(!stack.isEmpty()){
+            invalid.add(stack.pollLast());
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<s.length(); i++){
+            if(invalid.contains(i)) continue;
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
     }
 }
